c69151d62e708d467c3ea5978972aadb1d429943
valentino-cachan <cachanvalentino1@gmail.com>
2023-08-22 13:50:26 -0300

    Maquetacion terminada de Re-Direccion. Coments

--- assets/example.js
+++ assets/example.js
@@ -3,11 +3,9 @@ const next = document.querySelector('.next')
 const slider = document.querySelector('.slider')
 
 prev.addEventListener('click', () => {
-    console.log('prev')
     slider.scrollLeft -= 750
 })
 
 next.addEventListener('click', () => {
-    console.log('next')
     slider.scrollLeft += 650
 })
